92861bc33ba3ce2521d08987ee7769276ce095ed
sakawejsza <63130114+sakawejsza@users.noreply.github.com>
2020-11-12 20:47:33 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,31 +1,100 @@
 #Skeleton for the CLI
 import click
 
-@click.command()
 
-@click.option('--md5', default= 'hello',
-              help= 'md5 command')
-@click.option('--factorial', default= '1',
-              help= 'factorial test')
-@click.option('--fibonacci', default= '1',
-              help= 'fibonacci test')
-@click.option('--is-prime', default= '1',
-              help= 'is-prime test')
-@click.option('--slack-alert', default= '1',
-              help= 'slack-alert test')
+@click.group()
+def cli():
+    pass
+	
+	
+@cli.command()
+@click.option('--phrase', default= 'hello',
+              help= 'The phrase to translate to md5')
+def md5(phrase):
+	"""This will translate a given phrase to md5"""
+    print 'md5 run'
+	print number
+    pass
+#####
+
+
+@cli.command()
+@click.option('--number', default= 1,
+              help= 'The number to find factorials of')
+def factorial(number):
+	"""This will calcuate the factorial of a number"""
+    pass
+#######
+
+
+
+@cli.command()
+@click.option('--number', default= 1,
+              help= 'The number to find fibonacci sequence of')
+def fibonacci(number):
+	"""This will return the fibonacci sequence of a number"""
+    pass
+#######
+
+
+@cli.command()
+@click.option('--number', default= 1,
+              help= 'The number to be checked if prime')
+def is_prime(number):
+	"""This will tell you if a given number is prime or not"""
+    pass
+#####
+
+
+
+@cli.command()
+@click.option('--message', default= '1',
+              help= 'The message to send in slack')
+def slack_alert(message):
+	"""This will send a slack alert and tell you if it sent sucessfully"""
+    pass
+#####
+
+
+
+@cli.command()
 @click.option('--post', default= '',
               help= 'post test')
+def post(post):
+	"""Insert a single entry into the database"""
+    pass
+####
+
+
+
+@cli.command()
 @click.option('--get', default= '',
               help= 'get test')
+def get(user_key):
+	"""Returns the entry associated with the key"""
+    pass
+#####
+
+
+
+@cli.command()
 @click.option('--put', default= '',
               help= 'put test')
+def put(put):
+	"""Updates the entry associated with the key with the value provided"""
+    pass
+#####
+
+
+
+@cli.command()
 @click.option('--delete', default= '',
               help= 'delete test')
+def delete(user_key):
+	"""Remove the entries associate with the keys provided."""
+    pass
+######
 
-
-def cli():
-  #Set this up to use the options above to run tests
-  #Will probably need 2 of these, one for integers and one for string
-  #Should run like the OG code, just with a few minor changes here and there to accomodate arguments
-  
-  
+ 
+if __name__ == '__main__':
+   cli()
